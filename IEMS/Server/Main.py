3be1647ddbd1fae28eef05f15fca8e20d9c3dc00
wongchuinhun <wong.hun@gmail.com>
2018-10-09 09:54:48 +0800

    Code clean up to use Constant file

--- IEMS/Server/Main.py
+++ IEMS/Server/Main.py
@@ -1,8 +1,5 @@
-mqttServer="iot.eclipse.org"
-mqttPort=1883
-
-channelSubs="sg/lift" #topic configure as as index in ES
-
+CLIENT_ID ="IMES_SERVER_1"
+from Helpers.Constant import *
 import paho.mqtt.client as mqtt
 from datetime import datetime
 from elasticsearch import Elasticsearch
@@ -10,32 +7,24 @@ es = Elasticsearch() #[{'host': 'localhost', 'port': 9200}]
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe(channelSubs)
+    client.subscribe(Constant.MQTT_TOPIC)
 
 def on_message(client, userdata, msg):
-    #print(msg.topic+" "+str(msg.payload))
     try:
-        doc = {
-             'author': 'kimchy',
-            'text': 'Elasticsearch: cool. bonsai cool.',
-            'timestamp': datetime.now(),
-        }
-
-	    #print(msg.payload)
-        es.index(index="sg", doc_type="external", body=msg.payload)
-        res = es.index(index=channelSubs, doc_type='tweet', id=1, body=doc)
+        #print(msg.payload)
+        res= es.index(index="sg", doc_type="sensor", body=msg.payload)
         print(res['result'])
     	
     except:
         prin("Error")
 	#es.index(index="my-index", doc_type="string", body={"topic" : msg.topic, "dataString" : msg.payload, "timestamp": datetime.utcnow()})
 
-client = mqtt.Client("imes-sevr-01")
+client = mqtt.Client(CLIENT_ID)
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect(mqttServer,mqttPort, 60)
+client.connect(Constant.MQTT_BROKER,Constant.MQTT_PORT, 120)
 client.loop_forever()
-print("subscribing ")
-client.subscribe(channelSubs)#subscribe
+print("Subscribing ")
+client.subscribe(Constant.MQTT_TOPIC)#subscribe
 
-s
+
